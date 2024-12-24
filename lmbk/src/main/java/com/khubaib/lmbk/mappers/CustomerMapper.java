c4c278c0defe5cddc54924f41ace26c1a7f70652
Khubaib Ahmad <khubaib.ahmad@visable.com>
2024-12-24 01:25:15 +0100

    added mappers

--- lmbk/src/main/java/com/khubaib/lmbk/mappers/CustomerMapper.java
+++ lmbk/src/main/java/com/khubaib/lmbk/mappers/CustomerMapper.java
@@ -1,5 +1,14 @@
 package com.khubaib.lmbk.mappers;
 
+import org.mapstruct.Mapper;
+
+import com.khubaib.lmbk.dto.CustomerDTO;
+import com.khubaib.lmbk.entities.Customer;
+
+@Mapper
 public interface CustomerMapper {
 
+    Customer customerDtoToCustomer(CustomerDTO cusomterDto);
+
+    CustomerDTO customerToCustomerDto(Customer customer);
 }
